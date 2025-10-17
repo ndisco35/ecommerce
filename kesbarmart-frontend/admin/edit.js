f8f5028684051869ae25a36291d87ecee82cbb0f
John Ndifon <89045707+ndisco35@users.noreply.github.com>
2025-10-17 11:45:05 +0100

    Update edit.js
    jbknk

--- kesbarmart-frontend/admin/edit.js
+++ kesbarmart-frontend/admin/edit.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3000/api"; 
+const API_BASE = "https://ecommerce-wjx0.onrender.com/api"; 
 const productList= document.getElementById("form-wrap");
 const token = localStorage.getItem("token");
 // Get query string params (id from URL)
